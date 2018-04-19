e3a85d5dd3256010ad1b8c3e4a7fbe379ef5b272
Constantin <mitides.constantin@gmail.com>
2018-04-19 09:33:46 -0400

    initial commit added header and notes

---
+++ Assignment6.java
@@ -0,0 +1,47 @@
+
+/*************************************************************************
+ *
+ *  Pace University
+ *
+ *  Course: CS 608 Algorithms and Computing Theory
+ *  Author: Constantin Mitides
+ *  Collaborators: PUT YOUR COLLABORATORS HERE, IF NONE, PUT NONE
+ *  References: PUT THE LINKS TO YOUR SOURCES HERE
+ *
+ *  Assignment: PUT THE ASSIGNMENT NUMBER HERE
+ *  Problem: PUT THE MAIN PROBLEM NAME HERE
+ *  Description: PUT A BRIEF DESCRIPTION HERE
+ *
+ *  Input: 
+ *  Output: 
+ *
+ *  Visible data fields:
+ *  COPY DECLARATION OF VISIBLE DATA FIELDS HERE
+ *
+ *  Visible methods:
+ *  COPY SIGNATURE OF VISIBLE METHODS HERE
+ *
+ *
+ *   Remarks
+ *   -------
+ *
+ *   PUT ALL NON-CODING ANSWERS HERE
+ *
+ *
+ *************************************************************************/
+import java.util.Scanner;
+
+public class Assignment6{
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("enter a number of nodes and a number of edges");
+        int nodes = scan.nextInt();
+        int edges = scan.nextInt(); 
+        
+        //measure DFS run times
+    }
+    //create an adjaceny list choosing the edges at random (keep in mind this is a directed graph)
+
+    //DFS 
+
+}
